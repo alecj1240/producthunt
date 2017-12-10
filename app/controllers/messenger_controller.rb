e6a93958348185507ea34f80bcbba82ea1e62ff7
Alec J <alecaej2002@gmail.com>
2017-12-10 12:35:37 -0800

    getting the ph page

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -7,7 +7,9 @@ class MessengerController < ApplicationController
   def receive_message
     @webhook = CGI::parse(request.raw_post)
     puts @webhook
-    testMessage = Messagehuman.sendMessage(@webhook["response_url"][0], "this is a message")
-    puts testMessage
+    testMessage = Messagehuman.sendMessage(@webhook["response_url"][0], "we are retreiving that info...")
+
+    @productHuntPage = HTTParty.get("https://www.producthunt.com/topics/tech")
+    puts @productHuntPage
   end
 end
