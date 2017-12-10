bb22f27e635ed8a9eb8b11883b3e607c871cd92a
Alec J <alecaej2002@gmail.com>
2017-12-10 12:23:26 -0800

    trying to send a message

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -5,5 +5,8 @@ class MessengerController < ApplicationController
 	require 'cgi'
 
   def receive_message
+    @webhook = CGI::parse(request.raw_post)
+    puts @webhook
+    Messagehuman.sendMessage(@webhook["response_url"][0], "this is a message")
   end
 end
--- app/models/messagehuman.rb
+++ app/models/messagehuman.rb
@@ -1,2 +1,9 @@
 class Messagehuman
+  require 'json'
+
+
+	def self.sendMessage(url, text)
+		HTTParty.post(url.to_s, :body => { :response_type => 'ephemeral', :text => text}.to_json,
+    :headers => { 'Content-Type' => 'application/json' })
+	end
 end
