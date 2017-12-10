2c79da7d27a07c3a6e718c8e5cbb01d8886a58f5
Alec J <alecaej2002@gmail.com>
2017-12-10 15:56:54 -0800

    stuff

--- app/models/messagehuman.rb
+++ app/models/messagehuman.rb
@@ -8,7 +8,7 @@ class Messagehuman
 	end
 
   def self.phapi()
-    HTTParty.get("/v1/me/feed",
-    :headers => { 'Content-Type' => 'application/json', 'Authorization' => "be69146902dfda36771878b6a76300ad539d04947cde370de5999aedaa084656", "Host" => "api.producthunt.com" })
+    HTTParty.get("https://api.producthunt.com/v1/me/feed",
+    :headers => { 'Content-Type' => 'application/json', 'Authorization' => "be69146902dfda36771878b6a76300ad539d04947cde370de5999aedaa084656"})
   end
 end
