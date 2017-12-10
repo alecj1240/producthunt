59c745455f0a0522eddfd68be0614179e420a993
Alec J <alecaej2002@gmail.com>
2017-12-10 12:12:14 -0800

    added messenger_controller and messagehuman model

---
+++ app/controllers/messenger_controller.rb
@@ -0,0 +1,9 @@
+class MessengerController < ApplicationController
+	protect_from_forgery with: :null_session
+	require 'httparty'
+	require 'json'
+	require 'cgi'
+
+  def receive_message
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#index'
+  post 'messenger/webhook' => 'messenger#receive_message'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
