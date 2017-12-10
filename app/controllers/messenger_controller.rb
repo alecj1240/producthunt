a6ac0decc4ae90a341e3a6b3e4a6ddec36305089
Alec J <alecaej2002@gmail.com>
2017-12-10 13:11:38 -0800

    splitting the titles

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -9,13 +9,16 @@ class MessengerController < ApplicationController
     puts @webhook
     testMessage = Messagehuman.sendMessage(@webhook["response_url"][0], "we are retreiving that info...")
 
+    # getting the product hunt page that day
     @phPage = HTTParty.get("https://www.producthunt.com/topics/tech")
     @phPage = @phPage.to_s
+    #trimming down the excess
     @phPageArray = @phPage.split('Today')
     @phPageArray.shift
     @phPageArray = @phPageArray[0]
     @phPageArray = @phPageArray.split('secondaryContent_cdfcd')
     @phPageArray.pop
-    puts @phPageArray
+    #putsing the page
+    puts @phPageArray.split('title_9ddaf')
   end
 end
