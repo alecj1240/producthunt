7fc1c916d9607f83638317c1a4aa9a763654334b
Alec J <alecaej2002@gmail.com>
2017-12-17 11:15:10 -0800

    fixed it

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -196,11 +196,7 @@ class MessengerController < ApplicationController
 
 
 	def done
-		puts "HERE"
-		puts params.inspect
-		puts "BOO"
 		thecode = params["code"]
-		puts thecode.inspect
 		if !thecode.nil?
 			puts "THE TOKEN!"
 			@theToken = HTTParty.get("https://slack.com/api/oauth.access?client_id=219592720864.285551398295&client_secret=6546a0cc240d1946aa66f60f6bbcacd3&code=#{thecode}")
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def done
+		thecode = params["code"]
+		if !thecode.nil?
+			puts "THE TOKEN!"
+			@theToken = HTTParty.get("https://slack.com/api/oauth.access?client_id=219592720864.285551398295&client_secret=6546a0cc240d1946aa66f60f6bbcacd3&code=#{thecode}")
+			puts @theToken
+		end
+	end
 end
