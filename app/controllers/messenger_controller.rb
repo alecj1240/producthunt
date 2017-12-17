7566cb597e57e2ed0a2d9dda8f45587507147b22
Alec J <alecaej2002@gmail.com>
2017-12-17 11:15:28 -0800

    all good

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -194,14 +194,4 @@ class MessengerController < ApplicationController
 		end
   end
 
-
-	def done
-		thecode = params["code"]
-		if !thecode.nil?
-			puts "THE TOKEN!"
-			@theToken = HTTParty.get("https://slack.com/api/oauth.access?client_id=219592720864.285551398295&client_secret=6546a0cc240d1946aa66f60f6bbcacd3&code=#{thecode}")
-			puts @theToken
-		end
-	end
-
 end
