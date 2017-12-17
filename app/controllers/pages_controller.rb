972d70f8062d77a85260135af70f2021ce12bcd0
Alec J <alecaej2002@gmail.com>
2017-12-17 11:18:25 -0800

    fixing bugs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
 
   def done
 		thecode = params["code"]
+    puts thecode
 		if !thecode.nil?
 			puts "THE TOKEN!"
 			@theToken = HTTParty.get("https://slack.com/api/oauth.access?client_id=219592720864.285551398295&client_secret=6546a0cc240d1946aa66f60f6bbcacd3&code=#{thecode}")
